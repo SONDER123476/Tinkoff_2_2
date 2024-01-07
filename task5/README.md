dbc416ae1d63286c5efdb72f9f855a8c4d0b14ae
SONDER123476 <102980464+SONDER123476@users.noreply.github.com>
2024-01-07 23:46:24 +0400

    Delete task5 directory

--- task5/README.md
+++
@@ -1,2 +0,0 @@
-Для запуска сервера  json-server -p 3001 --watch server/db.json
-Для запуска фронта  npm start
--- task5/src/pages/HomePage.jsx
+++
@@ -1,9 +0,0 @@
-const HomePage = () => {
-  return (
-    <div className="home">
-      <h1 className="home__title">Выберите фильм</h1>
-    </div>
-  );
-};
-
-export default HomePage;
